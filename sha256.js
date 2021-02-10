6b88c5dd7de4428131ec48e35f99fcc09c126fd4
D <76818592+dev-cn99@users.noreply.github.com>
2021-02-10 20:33:49 +0900

    Update sha256.js

--- sha256.js
+++ sha256.js
@@ -1,7 +1,7 @@
 const sha256 = (str) => {
-    let md = MessageDigest.getInstance('SHA-256');
-    md.update(string(str).getBytes('UTF-8'));
-    let b = new buf();
+    let md = java.security.MessageDigest.getInstance('SHA-256');
+    md.update(java.lang.String(str).getBytes('UTF-8'));
+    let b = new java.lang.StringBuffer();
     for (let i of md.digest()) {
         b.append(java.lang.Integer.toString((i & 0xff) + 0x100, 16).slice(1));
     }
