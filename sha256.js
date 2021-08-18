535899316012c549c22685cd3d685a0d31e7dfbb
nonever <76818592+nonever99@users.noreply.github.com>
2021-08-18 15:24:11 +0900

    Update sha256.js

--- sha256.js
+++ sha256.js
@@ -1,9 +1,8 @@
 const sha256 = (str) => {
     let md = java.security.MessageDigest.getInstance('SHA-256');
     md.update(java.lang.String(str).getBytes('UTF-8'));
-    let b = new java.lang.StringBuffer();
-    for (let i of md.digest()) {
-        b.append(java.lang.Integer.toString((i & 0xff) + 0x100, 16).slice(1));
-    }
+    let b = '';
+    for (let i of md.digest())
+        b += java.lang.Integer.toString((i & 0xff) + 0x100, 16).slice(1);
     return b;
 }
